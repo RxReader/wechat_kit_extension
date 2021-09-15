c95c74f1e7589ec1d6028dadd4b28d5b99c88ed3
Alex Li <github@alexv525.com>
2021-09-15 17:18:57 +0800

    🎉 ## 1.0.0-dev.1

---
+++ README.md
@@ -0,0 +1,10 @@
+# wechat_kit_extension
+
+[![pub package](https://img.shields.io/pub/v/wechat_kit_extension?logo=dart&style=flat-square)](https://pub.dev/packages/wechat_kit_extension)
+[![GitHub license](https://img.shields.io/github/license/RxReader/wechat_kit_extension?style=flat-square)](https://github.com/RxReader/wechat_kit_extension/blob/master/LICENSE)
+
+An extension package for wechat_kit.
+
+## Get started
+
+`flutter pub wechat_kit_extension` .
---
+++ lib/wechat_kit_extension.dart
@@ -0,0 +1,218 @@
+library wechat_kit_extension;
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'wechat_kit_extension.g.dart';
+
+/// The extension class for `wechat_kit`.
+class WechatExtension {
+  const WechatExtension._();
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// 微信 APP 授权登录 ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+  /// 获取 access_token（UnionID）
+  static Future<WechatAccessTokenResp> getAccessTokenUnionID({
+    required String appId,
+    required String appSecret,
+    required String code,
+  }) {
+    return _createFactory(
+      creator: _$WechatAccessTokenRespFromJson,
+      url: 'https://api.weixin.qq.com/sns/oauth2/access_token'
+          '?appid=$appId'
+          '&secret=$appSecret'
+          '&code=$code'
+          '&grant_type=authorization_code',
+    );
+  }
+
+  /// 刷新或续期 access_token 使用（UnionID）
+  static Future<WechatAccessTokenResp> refreshAccessTokenUnionID({
+    required String appId,
+    required String refreshToken,
+  }) {
+    return _createFactory(
+      creator: _$WechatAccessTokenRespFromJson,
+      url: 'https://api.weixin.qq.com/sns/oauth2/refresh_token'
+          '?appid=$appId'
+          '&grant_type=refresh_token'
+          '&refresh_token=$refreshToken',
+    );
+  }
+
+  /// 获取用户个人信息（UnionID）
+  static Future<WechatUserInfoResp> getUserInfoUnionID({
+    required String openId,
+    required String accessToken,
+  }) {
+    return _createFactory(
+      creator: _$WechatUserInfoRespFromJson,
+      url: 'https://api.weixin.qq.com/sns/userinfo'
+          '?access_token=$accessToken'
+          '&openid=$openId',
+    );
+  }
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// 微信 APP 扫码登录 ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+  /// 获取 access_token
+  static Future<WechatAccessTokenResp> getAccessToken({
+    required String appId,
+    required String appSecret,
+  }) {
+    return _createFactory(
+      creator: _$WechatAccessTokenRespFromJson,
+      url: 'https://api.weixin.qq.com/cgi-bin/token'
+          '?grant_type=client_credential'
+          '&appid=$appId'
+          '&secret=$appSecret',
+    );
+  }
+
+  /// 用上面的函数拿到的 access_token，获取 sdk_ticket
+  static Future<WechatTicketResp> getTicket({
+    required String accessToken,
+  }) {
+    return _createFactory(
+      creator: _$WechatTicketRespFromJson,
+      url: 'https://api.weixin.qq.com/cgi-bin/token'
+          'https://api.weixin.qq.com/cgi-bin/ticket/getticket'
+          '?access_token=$accessToken'
+          '&type=2',
+    );
+  }
+}
+
+/// Base WeChat API response model.
+abstract class WechatAPIResp {
+  const WechatAPIResp({required this.errorCode, this.errorMsg});
+
+  /// 成功
+  static const int errorCodeSuccess = 0;
+
+  /// -1	   系统繁忙，此时请开发者稍候再试
+  /// 0      请求成功
+  /// 40001	 AppSecret 错误或者 AppSecret 不属于这个公众号，请开发者确认 AppSecret 的正确性
+  /// 40002  请确保 grant_type 字段值为 client_credential
+  /// 40164  调用接口的 IP 地址不在白名单中，请在接口 IP 白名单中进行设置。（小程序及小游戏调用不要求 IP 地址在白名单内。）
+  @JsonKey(name: 'errcode', defaultValue: errorCodeSuccess)
+  final int errorCode;
+  @JsonKey(name: 'errmsg')
+  final String? errorMsg;
+
+  bool get isSuccessful => errorCode == errorCodeSuccess;
+
+  Map<String, dynamic> toJson();
+
+  @override
+  String toString() => const JsonEncoder.withIndent('  ').convert(toJson());
+}
+
+@JsonSerializable()
+class WechatAccessTokenResp extends WechatAPIResp {
+  const WechatAccessTokenResp({
+    required int errorCode,
+    String? errorMsg,
+    this.openid,
+    this.scope,
+    this.accessToken,
+    this.refreshToken,
+    this.expiresIn,
+  }) : super(errorCode: errorCode, errorMsg: errorMsg);
+
+  factory WechatAccessTokenResp.fromJson(Map<String, dynamic> json) =>
+      _$WechatAccessTokenRespFromJson(json);
+
+  final String? openid;
+  final String? scope;
+  final String? accessToken;
+  final String? refreshToken;
+  final int? expiresIn; // 单位：秒
+
+  @override
+  Map<String, dynamic> toJson() => _$WechatAccessTokenRespToJson(this);
+}
+
+@JsonSerializable()
+class WechatTicketResp extends WechatAPIResp {
+  const WechatTicketResp({
+    required int errorCode,
+    String? errorMsg,
+    this.ticket,
+    this.expiresIn,
+  }) : super(errorCode: errorCode, errorMsg: errorMsg);
+
+  factory WechatTicketResp.fromJson(Map<String, dynamic> json) =>
+      _$WechatTicketRespFromJson(json);
+
+  final String? ticket;
+  final int? expiresIn;
+
+  @override
+  Map<String, dynamic> toJson() => _$WechatTicketRespToJson(this);
+}
+
+@JsonSerializable()
+class WechatUserInfoResp extends WechatAPIResp {
+  const WechatUserInfoResp({
+    required int errorCode,
+    String? errorMsg,
+    this.openid,
+    this.nickname,
+    this.sex,
+    this.province,
+    this.city,
+    this.country,
+    this.headimgurl,
+    this.privilege,
+    this.unionid,
+  }) : super(errorCode: errorCode, errorMsg: errorMsg);
+
+  factory WechatUserInfoResp.fromJson(Map<String, dynamic> json) =>
+      _$WechatUserInfoRespFromJson(json);
+
+  final String? openid;
+  final String? nickname;
+  final int? sex; // 1为男性，2为女性
+  final String? province;
+  final String? city;
+  final String? country;
+  final String? headimgurl;
+  final List<dynamic>? privilege;
+  final String? unionid;
+
+  bool get isMale => sex == 1;
+
+  bool get isFemale => sex == 2;
+
+  @override
+  Map<String, dynamic> toJson() => _$WechatUserInfoRespToJson(this);
+}
+
+typedef _DataFactory<T extends WechatAPIResp> = T Function(
+  Map<String, dynamic>,
+);
+
+Future<T> _createFactory<T extends WechatAPIResp>({
+  required _DataFactory<T> creator,
+  required String url,
+}) async {
+  final Uri uri = Uri.parse(url);
+  final HttpClientRequest request = await HttpClient().getUrl(uri);
+  final HttpClientResponse response = await request.close();
+  if (response.statusCode == HttpStatus.ok) {
+    final String content = await utf8.decodeStream(response);
+    return creator(json.decode(content) as Map<String, dynamic>);
+  }
+  throw HttpException(
+    'HttpResponse statusCode: ${response.statusCode}, '
+    'reasonPhrase: ${response.reasonPhrase}.',
+  );
+}
